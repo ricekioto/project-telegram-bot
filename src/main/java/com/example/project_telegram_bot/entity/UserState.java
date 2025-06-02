62e10e8608744f0995645c8e773281d7ae37dfcd
mmkit <ataichukuev3@gmail.com>
2025-06-02 08:29:02 +0500

    update value

--- src/main/java/com/example/project_telegram_bot/entity/UserState.java
+++ src/main/java/com/example/project_telegram_bot/entity/UserState.java
@@ -1,8 +1,7 @@
 package com.example.project_telegram_bot.entity;
 
 public enum UserState {
+    START,
     AWAITING_NAME,
-    FOOD_DRINK_SELECTION,
-    PIZZA_TOPPINGS,
-    AWAITING_CONFIRMATION;
+
 }
