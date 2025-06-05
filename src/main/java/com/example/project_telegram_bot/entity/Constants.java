b59e49be5c5a056029f68a111855ca7b141c2693
mmkit <ataichukuev3@gmail.com>
2025-06-05 14:32:35 +0500

    init

--- src/main/java/com/example/project_telegram_bot/entity/Constants.java
+++ src/main/java/com/example/project_telegram_bot/entity/Constants.java
@@ -4,7 +4,5 @@ public class Constants {
     public static final String START_DESCRIPTION = "Привет, я Бот, который может отправлять сообщения на английском" +
             "языке.";
 
-    public static final String CHAT_STATES = "";
-
     public static final String START_TEXT = "Начать";
 }
