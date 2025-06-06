fc25c8c193ab31b3f95f296ec1a3c70d94e8143a
mmkit <ataichukuev3@gmail.com>
2025-06-06 16:16:50 +0500

    review

--- src/main/java/com/example/project_telegram_bot/service/ParsingService.java
+++ src/main/java/com/example/project_telegram_bot/service/ParsingService.java
@@ -6,20 +6,15 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @Service
 public class ParsingService {
 
     private String cssSelector = "fs-40px bold";
 
-    public String parsingHtml(Path pathFileHtml) throws IOException {
-        String html = Files.readString(pathFileHtml);
+    public String parsingHtml(String html) {
         Document doc = Jsoup.parse(html);
-        Elements elements = doc.select(cssSelector);
-
-        return elements.get(0).text();
-
+        Elements elements = doc.getElementsByClass(cssSelector);
+        return elements.getFirst().text();
     }
 }
