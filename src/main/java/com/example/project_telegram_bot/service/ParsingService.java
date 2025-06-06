4e1268e0b46788f138a935be2dedd9c873867e76
mmkit <ataichukuev3@gmail.com>
2025-06-06 11:56:27 +0500

    add class for parsing html file

---
+++ src/main/java/com/example/project_telegram_bot/service/ParsingService.java
@@ -0,0 +1,24 @@
+package com.example.project_telegram_bot.service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class ParsingService {
+
+    private String cssSelector = "fs-40px bold";
+
+    public String parsingHtml(Path pathFileHtml) throws IOException {
+        String html = Files.readString(pathFileHtml);
+        Document doc = Jsoup.parse(html);
+        Elements elements = doc.select(cssSelector);
+        return elements.toString();
+    }
+}
