2502f6fc80450db1b79794c706bf1dfa70c83934
mmkit <ataichukuev3@gmail.com>
2025-06-06 16:17:47 +0500

    add Service

---
+++ src/main/java/com/example/project_telegram_bot/service/RequestService.java
@@ -0,0 +1,26 @@
+package com.example.project_telegram_bot.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RequestService {
+    @Value("${url.english.generate-website}")
+    private String url;
+    private RestTemplate restTemplate;
+
+    public RequestService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getHtml() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            throw new RuntimeException(responseEntity.getStatusCode().toString());
+        }
+    }
+}
