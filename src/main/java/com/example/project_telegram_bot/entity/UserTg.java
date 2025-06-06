02e375ef3cf0396b0c5dc67a7c00073d84e9b804
mmkit <ataichukuev3@gmail.com>
2025-06-06 16:17:11 +0500

    review

--- src/main/java/com/example/project_telegram_bot/entity/UserTg.java
+++ src/main/java/com/example/project_telegram_bot/entity/UserTg.java
@@ -1,13 +1,21 @@
 package com.example.project_telegram_bot.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class UserTg {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
     @Column(nullable = false)
-    String name;
+    private Long chatId;
+
+
 }
