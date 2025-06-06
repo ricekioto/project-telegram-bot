37915d9ccc49b1e655cdfb37c5954ee39ef36a90
mmkit <ataichukuev3@gmail.com>
2025-06-06 11:03:42 +0500

    add Constants

--- src/main/java/com/example/project_telegram_bot/entity/Constants.java
+++ src/main/java/com/example/project_telegram_bot/entity/Constants.java
@@ -1,8 +1,9 @@
 package com.example.project_telegram_bot.entity;
 
 public class Constants {
-    public static final String START_DESCRIPTION = "Привет, я Бот, который может отправлять сообщения на английском" +
-            "языке.";
+    public static final String START_DESCRIPTION = "Привет, я Бот, который может отправлять сообщения на английском" + "языке по расписанию.";
 
-    public static final String START_TEXT = "Начать";
+    public static final String CHAT_STATES = "chatStates";
+
+    public static final String CHAT_CLOSE = "Чат закрыт";
 }
