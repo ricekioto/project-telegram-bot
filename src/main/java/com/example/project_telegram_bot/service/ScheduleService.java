8f676642ce1ef8cb877b818d29185a944a21e61f
mmkit <ataichukuev3@gmail.com>
2025-06-08 12:08:50 +0500

    add class

---
+++ src/main/java/com/example/project_telegram_bot/service/ScheduleService.java
@@ -0,0 +1,78 @@
+package com.example.project_telegram_bot.service;
+
+import com.example.project_telegram_bot.bot.Bot;
+import lombok.Getter;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.telegram.abilitybots.api.sender.SilentSender;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import java.util.List;
+
+@Getter
+@Service
+public class ScheduleService {
+    private SilentSender sender;
+    private SendMessage sendMessage;
+    private EnglishService englishService;
+    private ResponseHandler responseHandler;
+    private List<Long> every10Seconds;
+    private List<Long> every30Minutes;
+    private List<Long> every60Minutes;
+
+    public ScheduleService(EnglishService englishService, Bot bot) {
+        this.englishService = englishService;
+        this.responseHandler = bot.getResponseHandler();
+        this.sendMessage = responseHandler.getSendMessage();
+        this.sender = responseHandler.getSender();
+        every10Seconds = responseHandler.getEvery10Seconds();
+        every30Minutes = responseHandler.getEvery30Minutes();
+        every60Minutes = responseHandler.getEvery60Minutes();
+    }
+
+    @Async
+    @Scheduled(cron = "*/10 * * * * *")
+    public void interlvalTime10Second() {
+        try {
+            for (Long instance : every10Seconds) {
+                sendMessage.setChatId(instance);
+                String messageText = englishService.getSentence();
+                sendMessage.setText(messageText);
+                sender.execute(sendMessage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Async
+    @Scheduled(cron = "0 */30 * * * *")
+    public void interlvalTime30Minute() {
+        try {
+            for (Long instance : every30Minutes) {
+                sendMessage.setChatId(instance);
+                String messageText = englishService.getSentence();
+                sendMessage.setText(messageText);
+                sender.execute(sendMessage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Async
+    @Scheduled(cron = "0 0 * * * *")
+    public void interlvalTime60Minute() {
+        try {
+            for (Long instance : every60Minutes) {
+                sendMessage.setChatId(instance);
+                String messageText = englishService.getSentence();
+                sendMessage.setText(messageText);
+                sender.execute(sendMessage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
