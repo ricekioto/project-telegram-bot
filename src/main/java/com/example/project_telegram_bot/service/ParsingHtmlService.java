cc38adc5b8e9e6aa0e981824b89544f7ccba86f2
mmkit <ataichukuev3@gmail.com>
2025-06-09 07:26:08 +0500

    add method

---
+++ src/main/java/com/example/project_telegram_bot/service/ParsingHtmlService.java
@@ -0,0 +1,26 @@
+package com.example.project_telegram_bot.service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParsingHtmlService {
+
+    private String cssSelectorForGeneration = "fs-40px bold";
+    private String cssSelectorForTranslator = "translation-word translation-chunk";
+
+    public String parsingHtmlFromGenerationEnglishSentence(String html) {
+        Document doc = Jsoup.parse(html);
+        Elements elements = doc.getElementsByClass(cssSelectorForGeneration);
+        return elements.getFirst().text();
+    }
+
+    public String parsingHtmlFromTranslator(String html) {
+        Document doc = Jsoup.parse(html);
+        Elements elements = doc.getElementsByClass(cssSelectorForTranslator);
+        return elements.getFirst().text();
+    }
+
+}
