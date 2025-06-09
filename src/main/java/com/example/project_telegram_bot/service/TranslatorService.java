3de189c86e3d90a8e75e2e5023aca3fddd86f2fb
mmkit <ataichukuev3@gmail.com>
2025-06-09 07:27:03 +0500

    add new class

---
+++ src/main/java/com/example/project_telegram_bot/service/TranslatorService.java
@@ -0,0 +1,23 @@
+package com.example.project_telegram_bot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TranslatorService {
+    private final ParsingHtmlService parsingHtmlService;
+    private final ParsingUrlService parsingUrlService;
+    private final RequestService requestService;
+
+    public TranslatorService(ParsingHtmlService parsingHtmlService, ParsingUrlService parsingUrlService, RequestService requestService) {
+        this.parsingHtmlService = parsingHtmlService;
+        this.parsingUrlService = parsingUrlService;
+        this.requestService = requestService;
+    }
+
+    public String getTranslatedText(String text) {
+        String validateUrl = parsingUrlService.buildTranslationUrl(text);
+        String html = requestService.getHtml(validateUrl);
+        String translatedText = parsingHtmlService.parsingHtmlFromTranslator(html);
+        return translatedText;
+    }
+}
