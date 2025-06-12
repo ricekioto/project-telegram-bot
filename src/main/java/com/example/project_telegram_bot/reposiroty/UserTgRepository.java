af06dcbe33e3fe91c4a38149cbda679072c4973f
mmkit <ataichukuev3@gmail.com>
2025-06-12 07:06:17 +0500

    add repository

---
+++ src/main/java/com/example/project_telegram_bot/reposiroty/UserTgRepository.java
@@ -0,0 +1,13 @@
+package com.example.project_telegram_bot.reposiroty;
+
+import com.example.project_telegram_bot.entity.UserTg;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserTgRepository extends JpaRepository<UserTg, Long> {
+
+    UserTg findUserById(Long id);
+
+    boolean existsByChatId(Long chatId);
+}
