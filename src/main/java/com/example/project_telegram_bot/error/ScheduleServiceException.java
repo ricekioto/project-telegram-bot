75641f6fb98e6e82895776901a3fa8a3d0a4ad52
mmkit <ataichukuev3@gmail.com>
2025-06-12 07:05:00 +0500

    add custom exception

---
+++ src/main/java/com/example/project_telegram_bot/error/ScheduleServiceException.java
@@ -0,0 +1,12 @@
+package com.example.project_telegram_bot.error;
+
+public class ScheduleServiceException extends RuntimeException {
+
+    public ScheduleServiceException(String message) {
+        super(message);
+    }
+
+    public ScheduleServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
