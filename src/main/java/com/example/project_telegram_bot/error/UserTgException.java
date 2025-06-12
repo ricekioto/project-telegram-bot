8ddd33abf1506e7091243da208226fda9c44b1bd
mmkit <ataichukuev3@gmail.com>
2025-06-12 07:05:07 +0500

    add custom exception

---
+++ src/main/java/com/example/project_telegram_bot/error/UserTgException.java
@@ -0,0 +1,8 @@
+package com.example.project_telegram_bot.error;
+
+public class UserTgException extends RuntimeException {
+    public UserTgException(String message) {
+    }
+    public UserTgException(String message, Throwable cause) {
+    }
+}
