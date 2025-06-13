7669861e2fcb9db191e1880c23389892b841a2cb
mmkit <ataichukuev3@gmail.com>
2025-06-13 07:08:26 +0500

    add service

---
+++ src/main/java/com/example/project_telegram_bot/service/MarkdownV2Service.java
@@ -0,0 +1,29 @@
+package com.example.project_telegram_bot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MarkdownV2Service {
+
+    public static String escapeMarkdownV2(String text) {
+        String escapedText = text.replace("_", "\\_")
+                .replace("*", "\\*")
+                .replace("[", "\\[")
+                .replace("]", "\\]")
+                .replace("(", "\\(")
+                .replace(")", "\\)")
+                .replace("~", "\\~")
+                .replace("`", "\\`")
+                .replace(">", "\\>")
+                .replace("#", "\\#")
+                .replace("+", "\\+")
+                .replace("-", "\\-")
+                .replace("=", "\\=")
+                .replace("|", "\\|")
+                .replace("{", "\\{")
+                .replace("}", "\\}")
+                .replace(".", "\\.")
+                .replace("!", "\\!");
+        return escapedText;
+    }
+}
