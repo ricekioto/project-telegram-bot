8f24d1053c030e47b68f6d8cd30bcac4e29a1a87
mmkit <ataichukuev3@gmail.com>
2025-06-14 17:36:41 +0500

    review

--- src/main/java/com/example/project_telegram_bot/config/SecurityConfiguration.java
+++ src/main/java/com/example/project_telegram_bot/config/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.example.project_telegram_bot.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +13,8 @@ import static org.springframework.http.HttpMethod.GET;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
     @Bean
