3c3ff3168ac83a94cb44e44a77803acfaedb752a
mmkit <ataichukuev3@gmail.com>
2025-06-19 09:06:04 +0500

    add method

--- src/main/java/com/example/project_telegram_bot/service/RequestService.java
+++ src/main/java/com/example/project_telegram_bot/service/RequestService.java
@@ -1,5 +1,6 @@
 package com.example.project_telegram_bot.service;
 
+import com.example.project_telegram_bot.entity.SentenceResponse;
 import com.example.project_telegram_bot.error.RequestServiceException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,31 @@ public class RequestService {
         this.restTemplate = restTemplate;
     }
 
-    public String getEntity(String url) throws RequestServiceException {
+    public String getHtml(String url) throws RequestServiceException {
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
         if (responseEntity.getStatusCode().is2xxSuccessful()) {
             return responseEntity.getBody();
         } else {
             throw new RequestServiceException("Не удалось получить HTML. Код состояния: " + responseEntity.getStatusCode());
         }
+    }
+
+    public String get(String url) throws RequestServiceException {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            throw new RequestServiceException("Не удалось получить HTML. Код состояния: " + responseEntity.getStatusCode());
+        }
+    }
+
+    public SentenceResponse getSentenceResponse(String url) throws RequestServiceException {
+        SentenceResponse[] responseEntity = restTemplate.getForObject(url, SentenceResponse[].class);
+        if (responseEntity.length != 0) {
+            return responseEntity[0];
+        } else {
+            return null;
+        }
+
     }
 }
