37b3c7e58a1a898315e2b1395a018deae372b381
mmkit <ataichukuev3@gmail.com>
2025-06-20 16:43:35 +0500

    change dependency

--- src/main/java/com/example/project_telegram_bot/service/ScheduleService.java
+++ src/main/java/com/example/project_telegram_bot/service/ScheduleService.java
@@ -14,31 +14,33 @@ import java.util.List;
 @Getter
 @Service
 public class ScheduleService {
+    private final String URL_RANDOM_SENTENCE_CONTROLLER;
     private SilentSender sender;
-    private SendMessage sendMessage;
-    private EnglishRandomService englishRandomService;
     private ResponseHandlerService responseHandlerService;
+    private RequestService requestService;
     private List<Long> every10Seconds;
     private List<Long> every30Minutes;
     private List<Long> every60Minutes;
 
     public ScheduleService(EnglishRandomService englishRandomService, Bot bot) {
-        this.englishRandomService = englishRandomService;
         this.responseHandlerService = bot.getResponseHandler();
-        this.sendMessage = responseHandlerService.getSendMessage();
         this.sender = responseHandlerService.getSender();
+        this.requestService = responseHandlerService.getRequestService();
+        this.URL_RANDOM_SENTENCE_CONTROLLER = responseHandlerService.getURL_RANDOM_SENTENCE_CONTROLLER();
         every10Seconds = responseHandlerService.getEvery10Seconds();
         every30Minutes = responseHandlerService.getEvery30Minutes();
         every60Minutes = responseHandlerService.getEvery60Minutes();
     }
 
     @Async
-    @Scheduled(cron = "*/10 * * * * *")
+    @Scheduled(cron = "0 */5 * * * *")
     public void interlvalTime10Second() throws ScheduleServiceException {
-        if (every10Seconds.isEmpty()){
+        SendMessage sendMessage = new SendMessage();
+
+        if (every10Seconds.isEmpty()) {
             return;
         }
-        String messageText = englishRandomService.getSentence();
+        String messageText = requestService.get(URL_RANDOM_SENTENCE_CONTROLLER);
         for (Long instance : every10Seconds) {
             sendMessage.setChatId(instance);
             sendMessage.setText(messageText);
@@ -49,10 +51,12 @@ public class ScheduleService {
     @Async
     @Scheduled(cron = "0 */30 * * * *")
     public void interlvalTime30Minute() throws ScheduleServiceException {
-        if (every30Minutes.isEmpty()){
+        SendMessage sendMessage = new SendMessage();
+
+        if (every30Minutes.isEmpty()) {
             return;
         }
-        String messageText = englishRandomService.getSentence();
+        String messageText = requestService.get(URL_RANDOM_SENTENCE_CONTROLLER);
         for (Long instance : every30Minutes) {
             sendMessage.setChatId(instance);
             sendMessage.setText(messageText);
@@ -63,10 +67,12 @@ public class ScheduleService {
     @Async
     @Scheduled(cron = "0 0 * * * *")
     public void interlvalTime60Minute() throws ScheduleServiceException {
-        if (every60Minutes.isEmpty()){
+        SendMessage sendMessage = new SendMessage();
+
+        if (every60Minutes.isEmpty()) {
             return;
         }
-        String messageText = englishRandomService.getSentence();
+        String messageText = requestService.get(URL_RANDOM_SENTENCE_CONTROLLER);
         for (Long instance : every60Minutes) {
             sendMessage.setChatId(instance);
             sendMessage.setText(messageText);
