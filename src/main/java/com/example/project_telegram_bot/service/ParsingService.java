b61b03278c5518960b8b590c9c8246eddfb6fb2b
mmkit <ataichukuev3@gmail.com>
2025-06-20 13:35:17 +0500

    minor change

--- src/main/java/com/example/project_telegram_bot/service/ParsingService.java
+++ src/main/java/com/example/project_telegram_bot/service/ParsingService.java
@@ -1,19 +1,19 @@
 package com.example.project_telegram_bot.service;
 
 import com.example.project_telegram_bot.error.ElementNotFoundException;
-import com.example.project_telegram_bot.error.ParsingHtmlException;
+import com.example.project_telegram_bot.error.ParsingException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ParsingHtmlService {
+public class ParsingService {
 
     private String cssSelectorForGeneration = "fs-40px bold";
     private String cssSelectorForTranslator = "translation-word translation-chunk";
 
-    public String parsingHtmlFromGenerationEnglishSentence(String html) throws ParsingHtmlException {
+    public String parsingHtmlFromGenerationEnglishSentence(String html) throws ParsingException {
         Document doc = Jsoup.parse(html);
         Elements elements = doc.getElementsByClass(cssSelectorForGeneration);
         if (elements.isEmpty()) {
@@ -23,7 +23,7 @@ public class ParsingHtmlService {
         return text;
     }
 
-    public String parsingHtmlFromTranslator(String html) throws ParsingHtmlException {
+    public String parsingHtmlFromTranslator(String html) throws ParsingException {
         Document doc = Jsoup.parse(html);
         Elements elements = doc.getElementsByClass(cssSelectorForTranslator);
         if (elements.isEmpty()) {
