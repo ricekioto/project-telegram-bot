217768d7de951df53f8cff7c5b97f71d176638d2
mmkit <ataichukuev3@gmail.com>
2025-06-22 10:53:11 +0500

    review

--- src/main/java/com/example/project_telegram_bot/service/BuildingUrlService.java
+++ src/main/java/com/example/project_telegram_bot/service/BuildingUrlService.java
@@ -6,35 +6,23 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class BuildingUrlService {
-    @Value("${url.english.translate-website}")
-    private String translationUrl;
-    @Value("${url.english.generate-website}")
-    private String generatorUrl;
+    @Value("${url.translate-controller}")
+    private String translationControllerUrl;
+    @Value("${url.random-controller}")
+    private String generatorControllerUrl;
     private final String SOURCE_LANG = "en"; // Язык оригинала
     private final String TARGET_LANG = "ru";
-    private final String COUNT = "1";
-    private final String LANGUAGE = "eng";
 
 
-    public String getTranslationUrl(String text) {
-        String encodedText = UriComponentsBuilder.fromUriString(text).build().encode().toString();
-
-        String url = UriComponentsBuilder.fromUriString(translationUrl)
-                .queryParam("source_lang", SOURCE_LANG)
-                .queryParam("target_lang", TARGET_LANG)
-                .queryParam("text", encodedText)
+    public String getTranslationControllerUrl(String text) {
+        String url = UriComponentsBuilder.fromUriString(translationControllerUrl)
+                .queryParam("text", text)
                 .build()
                 .toUriString();
-
         return url;
     }
 
-    public String getGeneraterUrl() {
-        String url = UriComponentsBuilder.fromUriString(generatorUrl)
-                .queryParam("language", LANGUAGE)
-                .queryParam("count", COUNT)
-                .build()
-                .toUriString();
-        return url;
+    public String getRandomEnglishControllerUrl() {
+        return generatorControllerUrl;
     }
 }
