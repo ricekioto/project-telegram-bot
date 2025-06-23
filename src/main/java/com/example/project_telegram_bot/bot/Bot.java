1ae7abde27311733793d6ef9da87b098847ec27a
mmkit <ataichukuev3@gmail.com>
2025-06-23 18:12:09 +0500

    review

--- src/main/java/com/example/project_telegram_bot/bot/Bot.java
+++ src/main/java/com/example/project_telegram_bot/bot/Bot.java
@@ -1,21 +1,21 @@
 package com.example.project_telegram_bot.bot;
 
 import com.example.project_telegram_bot.reposiroty.UserTgRepository;
-import com.example.project_telegram_bot.service.*;
+import com.example.project_telegram_bot.service.BuildingUrlService;
+import com.example.project_telegram_bot.service.KeyboardFactoryService;
+import com.example.project_telegram_bot.service.RequestService;
+import com.example.project_telegram_bot.service.ResponseHandlerService;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.bot.BaseAbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
 import org.telegram.abilitybots.api.objects.Flag;
-import org.telegram.abilitybots.api.objects.Locality;
 import org.telegram.abilitybots.api.objects.Reply;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.function.BiConsumer;
 
-import static com.example.project_telegram_bot.entity.Constants.CHAT_STATES;
 import static org.telegram.abilitybots.api.objects.Locality.ALL;
 import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
 import static org.telegram.abilitybots.api.util.AbilityUtils.getChatId;
