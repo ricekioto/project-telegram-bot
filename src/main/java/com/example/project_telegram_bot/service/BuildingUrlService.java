e2321530e4eccba29951fb171600b53c909dd3ee
mmkit <ataichukuev3@gmail.com>
2025-06-24 12:23:35 +0500

    review

--- src/main/java/com/example/project_telegram_bot/service/BuildingUrlService.java
+++ src/main/java/com/example/project_telegram_bot/service/BuildingUrlService.java
@@ -1,18 +1,19 @@
 package com.example.project_telegram_bot.service;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@Getter
 @Service
 public class BuildingUrlService {
     @Value("${url.translate-controller}")
     private String translationControllerUrl;
     @Value("${url.random-controller}")
     private String generatorControllerUrl;
-    private final String SOURCE_LANG = "en"; // Язык оригинала
+    private final String SOURCE_LANG = "en";
     private final String TARGET_LANG = "ru";
-
 
     public String getTranslationControllerUrl(String text) {
         String url = UriComponentsBuilder.fromUriString(translationControllerUrl)
@@ -20,9 +21,5 @@ public class BuildingUrlService {
                 .build()
                 .toUriString();
         return url;
-    }
-
-    public String getRandomEnglishControllerUrl() {
-        return generatorControllerUrl;
     }
 }
