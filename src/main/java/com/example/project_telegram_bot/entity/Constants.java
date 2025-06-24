0840a0d6e13cfa697a8f36cbd4f461ca67d8b0a6
mmkit <ataichukuev3@gmail.com>
2025-06-24 12:23:41 +0500

    review

--- src/main/java/com/example/project_telegram_bot/entity/Constants.java
+++ src/main/java/com/example/project_telegram_bot/entity/Constants.java
@@ -5,8 +5,10 @@ public class Constants {
     public static final String CHAT_CLOSE = "Бот остановлен";
     public static final String ANOTHER_ANSWER = "У меня нет ответа на этот случай." +
             " Выбери из предложенных вариантов.";
+    public static final String STARTED_MESSAGE = "Нажми на кнопку \"Получить\" для получения " +
+            "сгенерированного предложения на английском языке.\n" +
+            "Или можешь выбрать время c которым будет отправляться сообщение.";
     public static final String CHATS_EVERY_10_SECONDS = "every10Seconds";
     public static final String CHATS_EVERY_30_MINUTES = "every30Minutes";
     public static final String CHATS_EVERY_60_MINUTES = "every60Minutes";
-
 }
