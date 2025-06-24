86747ce0810295641ccd47173f49ca93e3f7516a
mmkit <ataichukuev3@gmail.com>
2025-06-24 12:23:49 +0500

    review

--- src/main/java/com/example/project_telegram_bot/service/RequestService.java
+++ src/main/java/com/example/project_telegram_bot/service/RequestService.java
@@ -1,18 +1,15 @@
 package com.example.project_telegram_bot.service;
 
 import com.example.project_telegram_bot.error.RequestServiceException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class RequestService {
     private final RestTemplate restTemplate;
-
-    public RequestService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
 
     public String get(String url) throws RequestServiceException {
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
@@ -22,5 +19,4 @@ public class RequestService {
             throw new RequestServiceException("Не удалось получить HTML. Код состояния: " + responseEntity.getStatusCode());
         }
     }
-
 }
