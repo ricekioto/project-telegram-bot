8229e2fcef1f1e663c557c3bec3530021146d539
mmkit <ataichukuev3@gmail.com>
2025-07-08 07:23:55 +0500

    review

--- src/main/java/com/example/project_telegram_bot/error/UserTgException.java
+++ src/main/java/com/example/project_telegram_bot/error/UserTgException.java
@@ -3,6 +3,7 @@ package com.example.project_telegram_bot.error;
 public class UserTgException extends RuntimeException {
     public UserTgException(String message) {
     }
+
     public UserTgException(String message, Throwable cause) {
     }
 }
