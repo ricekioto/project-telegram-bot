e029336520f551206a30de216de69fb43db4928f
mmkit <ataichukuev3@gmail.com>
2025-07-12 17:34:12 +0500

    review

--- src/main/java/com/example/project_telegram_bot/entity/Constants.java
+++ src/main/java/com/example/project_telegram_bot/entity/Constants.java
@@ -11,4 +11,5 @@ public class Constants {
     public static final String CHATS_EVERY_10_SECONDS = "every10Seconds";
     public static final String CHATS_EVERY_30_MINUTES = "every30Minutes";
     public static final String CHATS_EVERY_60_MINUTES = "every60Minutes";
+
 }
