7549f09ca01542616ab79edda42f8d78878ae4fb
mmkit <ataichukuev3@gmail.com>
2025-08-05 15:06:54 +0500

    review

--- src/main/java/com/example/project_telegram_bot/error/ScheduleServiceException.java
+++ src/main/java/com/example/project_telegram_bot/error/ScheduleServiceException.java
@@ -9,6 +9,7 @@ public class ScheduleServiceException extends RuntimeException {
     public ScheduleServiceException(String message, Throwable cause) {
         super(message, cause);
     }
+
 }
 
 
