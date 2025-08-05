d81ddbddfd65d03e81a7d27b239cc9ccb36bf2f4
mmkit <ataichukuev3@gmail.com>
2025-08-05 15:07:13 +0500

    review

--- src/main/java/com/example/project_telegram_bot/reposiroty/UserTgRepository.java
+++ src/main/java/com/example/project_telegram_bot/reposiroty/UserTgRepository.java
@@ -2,7 +2,11 @@ package com.example.project_telegram_bot.reposiroty;
 
 import com.example.project_telegram_bot.entity.UserTg;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserTgRepository extends JpaRepository<UserTg, Long> {
@@ -11,8 +15,15 @@ public interface UserTgRepository extends JpaRepository<UserTg, Long> {
 
     UserTg findByChatId(Long chatId);
 
+
     void deleteByChatId(Long chatId);
-}
+
+    List<UserTg> findAllByInterval(Byte interval);
+
+    @Query("update UserTg u set u.interval = :interval where u.chatId = :chatId")
+    @Modifying
+    UserTg updateIntervalByChatId(Byte interval, Long chatId);
+
 
 
 
