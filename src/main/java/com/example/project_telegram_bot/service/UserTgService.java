5812100c3f073f4d4137c19a3579f523eb5a686a
mmkit <ataichukuev3@gmail.com>
2025-08-05 15:07:16 +0500

    review

--- src/main/java/com/example/project_telegram_bot/service/UserTgService.java
+++ src/main/java/com/example/project_telegram_bot/service/UserTgService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserTgService {
@@ -30,6 +32,16 @@ public class UserTgService {
     @Transactional
     public void deleteByChatId(Long chatId) throws UserTgException {
         userTgRepository.deleteByChatId(chatId);
+    }
+
+    @Transactional
+    public List<UserTg> findAllByChatId(Byte interval) throws UserTgException {
+        return userTgRepository.findAllByInterval(interval);
+    }
+
+    @Transactional
+    public UserTg updateIntervalByChatId(Byte interval, Long chatId) {
+        return userTgRepository.updateIntervalByChatId(interval, chatId);
     }
 }
 
