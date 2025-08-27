39a153ca37d9ae1a22c049c6b153f5c02679e6ba
mmkit <ataichukuev3@gmail.com>
2025-08-27 19:49:25 +0500

    add dto

---
+++ src/main/java/com/example/project_telegram_bot/kafka/dto/SentenceGenerationRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.project_telegram_bot.kafka.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SentenceGenerationRequestDto {
+    private UUID requestId;
+}
