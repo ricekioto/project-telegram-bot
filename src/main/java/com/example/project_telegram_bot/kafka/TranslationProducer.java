d82f4bf26ee1f5d52f474bd3d10655afbc0bd20d
mmkit <ataichukuev3@gmail.com>
2025-09-08 10:57:17 +0500

    review

---
+++ src/main/java/com/example/project_telegram_bot/kafka/TranslationProducer.java
@@ -0,0 +1,23 @@
+package com.example.project_telegram_bot.kafka;
+
+import com.example.project_telegram_bot.kafka.dto.KafkaMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TranslationProducer {
+
+    @Value("${kafka.topic.translation.request}")
+    private String translationRequestTopic;
+
+    private final KafkaTemplate<String, KafkaMessage> kafkaTemplate;
+
+
+    public void sendTranslationRequest(KafkaMessage message) {
+        kafkaTemplate.send(translationRequestTopic, message);
+    }
+
+}
