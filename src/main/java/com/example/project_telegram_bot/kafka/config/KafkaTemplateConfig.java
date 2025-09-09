b0ab6c9c212aed5f3e24834128a0fe52a7ea93d9
mmkit <ataichukuev3@gmail.com>
2025-09-09 14:39:51 +0500

    review

--- src/main/java/com/example/project_telegram_bot/kafka/config/KafkaTemplateConfig.java
+++ src/main/java/com/example/project_telegram_bot/kafka/config/KafkaTemplateConfig.java
@@ -1,53 +1,80 @@
-package com.example.project_telegram_bot.config; // Предполагаемый пакет
+package com.example.project_telegram_bot.kafka.config; // Предполагаемый пакет
 
+import com.example.project_telegram_bot.kafka.dto.KafkaMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaTemplateConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    private final ObjectMapper objectMapper;
+
     @Bean
-    public ProducerFactory<String, Object> sentenceRequestProducerFactory() {
+    public ProducerFactory<String, KafkaMessage> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-        return new DefaultKafkaProducerFactory<>(configProps);
+
+        JsonSerializer<KafkaMessage> jsonSerializer = new JsonSerializer<>(objectMapper);
+
+        DefaultKafkaProducerFactory<String, KafkaMessage> pf = new DefaultKafkaProducerFactory<>(configProps,
+                new StringSerializer(),
+                jsonSerializer);
+        return pf;
     }
 
     @Bean
-    public KafkaTemplate<String, Object> sentenceRequestKafkaTemplate(
-            ProducerFactory<String, Object> sentenceRequestProducerFactory) {
-        return new KafkaTemplate<>(sentenceRequestProducerFactory);
+    public KafkaTemplate<String, KafkaMessage> kafkaTemplate(
+            ProducerFactory<String, KafkaMessage> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 
     @Bean
-    public ProducerFactory<String, Object> translationRequestProducerFactory() {
+    public ConsumerFactory<String, KafkaMessage> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-        return new DefaultKafkaProducerFactory<>(configProps);
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        configProps.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+
+        JsonDeserializer<KafkaMessage> jsonDeserializer = new JsonDeserializer<>(KafkaMessage.class, objectMapper);
+
+        DefaultKafkaConsumerFactory<String, KafkaMessage> cf = new DefaultKafkaConsumerFactory<>(configProps,
+                new StringDeserializer(),
+                jsonDeserializer);
+        return cf;
     }
 
     @Bean
-    public KafkaTemplate<String, Object> translationRequestKafkaTemplate(
-            ProducerFactory<String, Object> translationRequestProducerFactory) {
-        return new KafkaTemplate<>(translationRequestProducerFactory);
+    public ConcurrentKafkaListenerContainerFactory<String, KafkaMessage> kafkaConsumerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, KafkaMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 }
+
