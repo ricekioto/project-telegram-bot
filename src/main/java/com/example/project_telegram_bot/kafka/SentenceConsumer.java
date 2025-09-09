9d608d7d4e4bb66911baf91ed9daa6550950ee10
mmkit <ataichukuev3@gmail.com>
2025-09-09 14:52:50 +0500

    review

--- src/main/java/com/example/project_telegram_bot/kafka/SentenceConsumer.java
+++ src/main/java/com/example/project_telegram_bot/kafka/SentenceConsumer.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SentenceConsumer {
-
     @Value("${kafka.topic.sentence.response}")
     private String sentenceResponseTopic;
 
